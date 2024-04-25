9871630e2845f8843cef0efed29fc94d8e666bdf
Maxime DROUAULT <119673669+maximedrouault@users.noreply.github.com>
2024-04-25 10:19:50 +0200

    Initial commit

---
+++ src/test/java/com/nnk/springboot/PoseidonCapitalSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnk.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoseidonCapitalSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
