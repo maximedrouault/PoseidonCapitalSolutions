70475665a12059f4a41527841180bae7727497c7
Maxime DROUAULT <119673669+maximedrouault@users.noreply.github.com>
2024-04-30 11:32:12 +0200

    Implementation of methods and validation of RuleName

--- src/main/java/com/nnk/springboot/domain/RuleName.java
+++ src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -1,7 +1,9 @@
 package com.nnk.springboot.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 @Table(name = "rulename")
@@ -12,10 +14,27 @@ public class RuleName {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotBlank(message = "Name is mandatory")
+    @Length(max = 125)
     private String name;
+
+    @NotBlank(message = "Description is mandatory")
+    @Length(max = 125)
     private String description;
+
+    @NotBlank(message = "Json is mandatory")
+    @Length(max = 125)
     private String json;
+
+    @NotBlank(message = "Template is mandatory")
+    @Length(max = 512)
     private String template;
+
+    @NotBlank(message = "sqlStr is mandatory")
+    @Length(max = 125)
     private String sqlStr;
+
+    @NotBlank(message = "sqlPart is mandatory")
+    @Length(max = 125)
     private String sqlPart;
 }
